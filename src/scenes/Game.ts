b13d86074bff0c03a456ea3933b9897dfab4ca34
nathan <jonathan@ouebsson.fr>
2024-05-30 19:10:51 +0200

    NEW: déplacement au clavier du poisson

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -4,6 +4,7 @@ export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     fishPaddle: Phaser.Physics.Arcade.Image;
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 
     constructor() {
         super('Game');
@@ -17,6 +18,21 @@ export class Game extends Scene {
         this.background.setAlpha(0.5);
 
         this.createFishPaddle();
+        if (this.input.keyboard) {
+            this.cursors = this.input.keyboard.createCursorKeys();
+        }
+    }
+
+    update() {
+        if (this.cursors.left.isDown) {
+            this.fishPaddle.setVelocityX(-300);
+            this.fishPaddle.setFlipX(true); // à gauche on le retourne
+        } else if (this.cursors.right.isDown) {
+            this.fishPaddle.setVelocityX(300);
+            this.fishPaddle.setFlipX(false); // à droite on le remet à l'endroit
+        } else {
+            this.fishPaddle.setVelocityX(0);
+        }
     }
 
     private createFishPaddle() {
@@ -29,7 +45,5 @@ export class Game extends Scene {
 
         this.fishPaddle.setCollideWorldBounds(true);
         this.fishPaddle.setImmovable(true);
-
-        this.fishPaddle.setVelocity(0, -100);
     }
 }
