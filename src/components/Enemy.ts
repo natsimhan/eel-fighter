1c025dfe68fb4a99ff2f72b60a0487b0b1faac5d
nathan <jonathan@ouebsson.fr>
2024-06-02 11:21:55 +0200

    NEW: Oscillation verticale des ennemies

--- src/components/Enemy.ts
+++ src/components/Enemy.ts
@@ -2,6 +2,9 @@ import Phaser from 'phaser';
 
 export class Enemy extends Phaser.GameObjects.Image {
     velocityX: number;
+    velocityY: number;
+    limitTop: number;
+    limitBottom: number;
 
     constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
         super(scene, x, y, 'fishPack', texture);
@@ -11,13 +14,31 @@ export class Enemy extends Phaser.GameObjects.Image {
         const scale = Phaser.Math.FloatBetween(0.5, scaleMax);
         this.setScale(scale);
         this.setOrigin(0, .5);
+
         this.velocityX = -50 - 150 * (scaleMax - scale);
+        this.velocityY = (Phaser.Math.Between(0, 1) ? 1 : -1) * 100 * (scaleMax - scale);
+
+        // Le 50 ici doit être inférieur à la limite haute et basse du spawn,
+        // afin que les oscillations verticales ne sortent pas de l'écran.
+        const velocityAmplitudeY = Phaser.Math.Between(0, 50);
+        this.limitTop = y - velocityAmplitudeY;
+        this.limitBottom = y + velocityAmplitudeY;
     }
 
     update(delta: number): void {
         this.x += this.velocityX * delta / 1000;
         if (this.x < -this.getBounds().width) {
             this.destroy();
+            return;
+        }
+
+        this.y += this.velocityY * delta / 1000;
+        if (this.y < this.limitTop) {
+            this.y = this.limitTop;
+            this.velocityY = -this.velocityY;
+        } else if (this.y > this.limitBottom) {
+            this.y = this.limitBottom;
+            this.velocityY = -this.velocityY;
         }
     }
 }
